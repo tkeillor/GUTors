9a6acab87b72dbcc4ea1e204af6044ce042441b3
Thomas Keillor <2888969k@student.gla.ac.uk>
2025-03-07 12:06:12 +0000

    Model and admin set up.

--- GUTors_app/admin.py
+++ GUTors_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from GUTors_app.models import Student, Tutor, Booking, Review
 
-# Register your models here.
+admin.site.register(Student)
+admin.site.register(Tutor)
+admin.site.register(Booking)
+admin.site.register(Review)
--- GUTors_app/models.py
+++ GUTors_app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    StudentID = models.CharField(max_length=8, unique=True)
+    name = models.CharField(max_length=60)
+    email = models.CharField(max_length=60)
+    description = models.CharField(max_length=5000)
+
+    def __str__(self):
+        return self.name
+    
+class Tutor(models.Model):
+    TutorID = models.CharField(max_length=8, unique=True)
+    name = models.CharField(max_length=60)
+    email = models.CharField(max_length=60)
+    description = models.CharField(max_length=5000)
+    subjects = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+    
+class Booking(models.Model):
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
+    BookingID = models.CharField(max_length=8, unique=True)
+    date = models.DateField()
+    numOfStudents = models.IntegerField()
+    description = models.CharField(max_length=1000)
+    subjects = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.BookingID
+    
+class Review(models.Model):
+    author = models.ForeignKey(Student, on_delete=models.CASCADE)
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
+    ReviewID = models.CharField(max_length=8, unique=True)
+    rating = models.IntegerField()
+    content = models.CharField(max_length=5000)
+
+    def __str__(self):
+        return self.ReviewID
