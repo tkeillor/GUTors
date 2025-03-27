79c743f0da5eafa93bbaebe05272118647159f0f
Gareth Edwards <2792803E@student.gla.ac.uk>
2025-03-27 12:35:48 +0000

    Css and html formatting for register form

--- GUTors_app/tests/test_views.py
+++ GUTors_app/tests/test_views.py
@@ -16,7 +16,9 @@ class TestProfileView(TestCase):
         '''Creating test user'''
         self.client = Client()
         self.user = User.objects.create_user(username='testuser', password='ExamplePassword123')
+        self.subject = Subject.objects.create(name="Mathematics")
         self.user_profile = UserProfile.objects.create(user=self.user, bio="Test Bio")
+        self.user_profile.subjects.add(self.subject)
 
         
     def test_profile_view_logged_in(self):
@@ -33,12 +35,38 @@ class TestProfileView(TestCase):
             reverse('GUTors:profile', kwargs={'username': 'testuser'}),
             {'bio': 'Updated Bio', 'role': 'STUDENT'}
         )
-        self.assertEqual(response.status_code,302)#Redirects user after successful form submission
+        self.assertEqual(response.status_code,200)#Redirects user after successful form submission
         self.user_profile.refresh_from_db()
         self.assertEqual(self.user_profile.bio, 'Updated Bio')  
-        print(response.content.decode())      
+        print(response.content.decode())     
         
-  
+    def test_register_profile_exists(self):
+        self.client.login(username='testuser', password='ExamplePassword123')
+        response = self.client.get(reverse('GUTors:Register_profile'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'GUTors_app/profile_registration.html')
+        
+class TestSearchView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.subject = Subject.objects.create(name="Physics")
+        self.user = User.objects.create_user(username="physics_tutor", password="password")
+        self.user_profile = UserProfile.objects.create(user=self.user)
+        self.user_profile.subjects.add(self.subject)
+            
+    def test_search_by_username(self):
+        response = self.client.get(reverse('GUTors:search'), {'username': 'physics_tutor'})  
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "physics_tutor") 
+        
+    def test_searhc_by_subject(self):
+        
+        response = self.client.get(reverse('GUTors:Search'), {'subject':self.subject.id})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Physics")
+
+
+
 #response status codes for unit tests 
 # 1xx - Informational
 # 100: Continue - Request received, continue sending
@@ -65,4 +93,4 @@ class TestProfileView(TestCase):
 # 500: Internal Server Error - Unexpected server issue
 # 502: Bad Gateway - Invalid response from upstream server
 # 503: Service Unavailable - Server overloaded or down
-# 504: Gateway Timeout - Upstream server took too long to respond      
+# 504: Gateway Timeout - Upstream server took too long to respond
